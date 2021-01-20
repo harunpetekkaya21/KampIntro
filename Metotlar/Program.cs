c33ca661816adf05df165f5d488e766ea8f0a825
Harun Petekkaya <harunpe2107@gmail.com>
2021-01-20 22:22:00 +0300

    KampIntro

---
+++ Metotlar/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+            urun1.Adi = "elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elmasi";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakir Karpuzu";
+
+            Urun[] urunler = new Urun[] {urun1,urun2 };
+
+            foreach (var item in urunler)
+            {
+                Console.WriteLine(item.Adi);
+                Console.WriteLine(item.Fiyati);
+                Console.WriteLine(item.Aciklama);
+                Console.WriteLine("--------");
+            }
+
+            Console.WriteLine("---------Metotlar---------------");
+            //Instance -class ornegi
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+       
+           
+
+
+        }
+    }
+}
---
+++ OOP1/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class Product //Entity
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        //birim fiyati
+        public double UnitPrice { get; set; }   
+
+        //stoktaki birim-stok  adedi
+        public int UnitInStock { get; set; }
+    }
+}
