53e605ff3e5f1edea2ffdab3aa81044b95cc9b60
Harun Petekkaya <harunpe2107@gmail.com>
2021-01-21 09:41:23 +0300

    Generics

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> islemler = new MyList<string>();
+            islemler.Add("engin");
+            Console.WriteLine();
+        }
+    }
+}
--- OOP1/ProductManager.cs
+++ OOP1/ProductManager.cs
@@ -8,7 +8,12 @@ namespace OOP1
     {
         public void Add(Product product)
         {
+            Console.WriteLine(product.ProductName + " eklendi.");
+        }
 
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " guncellendi.");
         }
     }
 }
